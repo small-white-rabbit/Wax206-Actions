0ec783d7f8fb1780afede1e12f2a847920033cae
small-white-rabbit <525307811@qq.com>
2026-03-29 23:51:50 +0800

    Update mt7622-256m.mk

--- mediatek/image/mt7622-256m.mk
+++ mediatek/image/mt7622-256m.mk
@@ -65,7 +65,7 @@ define Device/netgear_wax206
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 14436k
-  IMAGE_SIZE := 262144k        # ← 修改：256M
+  IMAGE_SIZE := 262144k
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | \
 	append-squashfs4-fakeroot
 # recovery can also be used with stock firmware web-ui, hence the padding...
