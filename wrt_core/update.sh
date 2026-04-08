ed7c414d203c5bdb5fdf2634051d1d3b9792073b
small-white-rabbit <525307811@qq.com>
2026-04-09 07:42:08 +0800

    Update update.sh

--- wrt_core/update.sh
+++ wrt_core/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# 开启严格模式，但我们会通过逻辑控制防止误杀
 set -e
 set -o errexit
 set -o errtrace
@@ -16,27 +15,26 @@ REPO_BRANCH=$2
 BUILD_DIR=$3
 COMMIT_HASH=$4
 
-# 转换为绝对路径
+# Convert BUILD_DIR to absolute path
 if [[ "$BUILD_DIR" != /* ]]; then
     BUILD_DIR="$(pwd)/$BUILD_DIR"
 fi
 
-# 核心变量设置
 FEEDS_CONF="feeds.conf.default"
 GOLANG_REPO="https://github.com/sbwml/packages_lang_golang"
-GOLANG_BRANCH="26.x"
+GOLANG_BRANCH="25.x"
 THEME_SET="argon"
 LAN_ADDR="192.168.31.1"
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 BASE_PATH=${BASE_PATH:-$SCRIPT_DIR}
 
-# 引入模块
 source "$SCRIPT_DIR/modules/general.sh"
 source "$SCRIPT_DIR/modules/feeds.sh"
 source "$SCRIPT_DIR/modules/packages.sh"
 source "$SCRIPT_DIR/modules/system.sh"
-# source "$SCRIPT_DIR/modules/cups.sh" # 打印机依赖复杂，建议精简
+source "$SCRIPT_DIR/modules/cups.sh"
+
 
 main() {
     clone_repo
@@ -45,18 +43,22 @@ main() {
     update_feeds
     remove_unwanted_packages
     remove_tweaked_packages
+    update_homeproxy
     fix_default_set
     fix_miniupnpd
     update_golang
     change_dnsmasq2full
     fix_mk_def_depends
 
+    # install_libubox_cmake_patch
     update_default_lan_addr
+    remove_something_nss_kmod
     update_affinity_script
     update_ath11k_fw
     # fix_mkpkg_format_invalid
     change_cpuusage
     update_tcping
+    add_ax6600_led
     set_custom_task
     apply_passwall_tweaks
     update_nss_pbuf_performance
@@ -66,14 +68,16 @@ main() {
     fix_compile_coremark
     update_dnsmasq_conf
     add_backup_info_to_sysupgrade
+    update_mosdns_deconfig
     fix_quickstart
     update_oaf_deconfig
+    add_timecontrol
     add_quickfile
 #    update_lucky
     fix_rust_compile_error
-    #update_smartdns
-    #update_diskman
-    #update_dockerman
+    update_smartdns
+    update_diskman
+    update_dockerman
     set_nginx_default_config
     update_uwsgi_limit_as
     update_argon
@@ -84,21 +88,21 @@ main() {
     remove_attendedsysupgrade
     fix_kconfig_recursive_dependency
     install_feeds
-    #update_docker_stack
-    #fix_cups_libcups_avahi_depends
+    fix_cups_libcups_avahi_depends
     fix_easytier_lua
-    #update_adguardhome
+    update_adguardhome
     update_script_priority
     update_geoip
     fix_openssl_ktls
     fix_opkg_check
     fix_quectel_cm
-    #install_pbr_cmcc
-    #fix_pbr_ip_forward
+    install_pbr_cmcc
+    fix_pbr_ip_forward
+    update_package "runc" "releases" "v1.3.3"
+    update_package "containerd" "releases" "v1.7.28"
+    update_package "docker" "tags" "v28.5.2"
+    update_package "dockerd" "releases" "v28.5.2"
     # apply_hash_fixes
-    
-    echo "✅ 所有预处理任务已完成！"
 }
 
-# 启动脚本
 main "$@"
